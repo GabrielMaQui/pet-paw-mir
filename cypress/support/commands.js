9798bb8a91c132ccaa2e41f120d1b60648ccf1e4
Gabriel Machicao Quispe <139248647+GabrielMaQui@users.noreply.github.com>
2024-11-26 20:36:52 -0500

    Feat/testing cypress details post (#54)
    * feat: configuration for cypress
    
    * feat: add complete test for login
    
    * feat: add invalidation for login
    
    * feat: add test for access feed
    
    * fix: refactor code for scheme and delete component unused
    
    * feat: add test feed and go to details for post
    
    * feat: add test for detailsPost with comment and info
    
    ---------
    
    Co-authored-by: Gabriel Steven Machicao Quispe <legard2911@gmail.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,43 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+
+  cy.visit('/login')
+
+  // Alias para elementos comunes
+  cy.get('input[placeholder="User or email"]').as('usernameInput');
+  cy.get('input[placeholder="Password"]').as('passwordInput');
+  cy.get('[aria-label="LOGIN"]').as('loginButton');
+
+  cy.get('@usernameInput').type(email);
+
+  // {enter} causes the form to submit
+  cy.get('@passwordInput').type(password);
+
+  cy.get('@loginButton').click();
+
+})
+
+Cypress.Commands.add('goToPostDetails', (postIndex = 1) => {
+  // Verifica que estamos en la página de Feed
+  cy.url().should('include', '/feed');
+
+  // Asegúrate de que haya suficientes publicaciones
+  cy.get('.grid > div').should('have.length.greaterThan', postIndex);
+
+  cy.get('.grid > div').should('have.length.greaterThan', 1);
+
+
+    cy.get('.grid > div').eq(1).click();
+    cy.url().should('match', /\/pet\/\d+$/);
+    cy.get('img[alt="Pet"]').should('be.visible');
+});
+
+
+
+
 //
 //
 // -- This is a child command --
@@ -22,4 +58,5 @@
 //
 //
 // -- This will overwrite an existing command --
+
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
